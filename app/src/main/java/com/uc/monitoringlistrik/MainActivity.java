03e390a35976aaffabe5fb52ced190ea02f1a749
Albar0o0 <66136746+Albar0o0@users.noreply.github.com>
2022-05-21 12:13:02 +0700

    iot

---
+++ app/src/main/java/com/uc/monitoringlistrik/MainActivity.java
@@ -0,0 +1,23 @@
+package com.uc.monitoringlistrik;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.v4.os.HandlerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+    }
+}
